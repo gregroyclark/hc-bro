cc3a0143d2c0c6c04f8851bea5ab314e931b1e12
peacefrog <gregroyclark@gmail.com>
2020-08-07 12:37:40 -0500

    comments out 10-12

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
+// }
 
 // app.use(routes);
 
