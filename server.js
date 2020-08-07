72b176d8079508392e39a4f81c82b5d49b6afe51
peacefrog <gregroyclark@gmail.com>
2020-08-07 12:25:25 -0500

    sets up server

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
